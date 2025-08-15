99998a5494cbcbd2c5fe759cbc57be5d4056378a
Balas Marius Andrei <balasandrei200@yahoo.com>
2025-08-15 21:11:11 +0000

    this version works.

--- scripts/data_fetcher.py
+++ scripts/data_fetcher.py
@@ -176,3 +176,5 @@ if __name__ == "__main__":
     tickers = sys.argv[1:] or ["AAPL", "MSFT", "ACQ.TO", "ANIP"]
     res = fetch_pe_for_top(tickers)
     print(json.dumps(res, indent=2))
+
+#IT JUST WORKS !
